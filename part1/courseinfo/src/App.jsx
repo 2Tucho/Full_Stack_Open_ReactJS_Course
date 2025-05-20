0a047c0c64a9de0bfc384b007cd2d290fe9d0a94
2Tucho <albergromero@gmail.com>
2025-05-20 13:03:58 +0200

    Added exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,26 +1,26 @@
-const Header = (props) => {
-  //Nombre del curso
-  return <h1>{props.course}</h1>
-}
+// const Header = (props) => {
+//   //Nombre del curso
+//   return <h1>{props.course}</h1>
+// }
 
-const Part = (props) => {
-  console.log(props.part.name)
-  return <p>{props.part.name} {props.part.exercises}</p>
-}
+// const Part = (props) => {
+//   console.log(props.part.name)
+//   return <p>{props.part.name} {props.part.exercises}</p>
+// }
 
-const Content = (props) => {
-  //Partes y su número de ejercicios
-  return <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-}
+// const Content = (props) => {
+//   //Partes y su número de ejercicios
+//   return <div>
+//     <Part part={props.parts[0]} />
+//     <Part part={props.parts[1]} />
+//     <Part part={props.parts[2]} />
+//   </div>
+// }
 
-const Total = (props) => {
-  //Número total de jercicios
-  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-}
+// const Total = (props) => {
+//   //Número total de jercicios
+//   return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+// }
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -41,9 +41,10 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h1>{course}</h1>
+      {parts.map((elem, index) => <p key={`parts${index}`}>{parts[index].name} {parts[index].exercises}</p>)}
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+      {/* <Total parts={parts} /> */}
     </div>
   )
 }
