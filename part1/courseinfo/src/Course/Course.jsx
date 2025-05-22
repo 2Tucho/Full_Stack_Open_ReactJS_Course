fd177ff23e921cfd85962e746e54fd4689fdc62b
2Tucho <albergromero@gmail.com>
2025-05-22 16:41:44 +0200

    Added exercise 2.3

--- part1/courseinfo/src/Course/Course.jsx
+++ part1/courseinfo/src/Course/Course.jsx
@@ -3,16 +3,16 @@ import Content from "./Content/Content";
 import Total from "./Total/Total";
 
 const Course = ({ course }) => {
-    let totalExercises = 0
-    for(let i = 0; i < course.parts.length; i++) {
-        totalExercises += course.parts[i].exercises
-    }
+
+    // for(let i = 0; i < course.parts.length; i++) {
+    //     totalExercises += course.parts[i].exercises
+    // }
 
     return (
         <div>
             <Header name={course.name} />
             <Content parts={course.parts} />
-            <Total total={totalExercises} />
+            <Total parts={course.parts} />
         </div>
     )
 }
--- part1/courseinfo/src/Course/Total/Total.jsx
+++ part1/courseinfo/src/Course/Total/Total.jsx
@@ -1,6 +1,7 @@
-const Total = ({ total }) => {
-    console.log(total)
-    return <p><strong>Total of {total} exercises</strong></p>
+const Total = ({ parts }) => {
+    const totalExercises = parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)
+
+    return <p><strong>Total of {totalExercises} exercises</strong></p>
 }
 
 export default Total
