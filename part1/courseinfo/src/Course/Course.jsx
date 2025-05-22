78410a58af171340f918058b52f103c25bbaf05f
2Tucho <albergromero@gmail.com>
2025-05-22 16:30:02 +0200

    Added exercise 2.2

--- part1/courseinfo/src/Course/Course.jsx
+++ part1/courseinfo/src/Course/Course.jsx
@@ -1,23 +1,20 @@
 import Header from "./Header/Header";
 import Content from "./Content/Content";
-
-//const Total = (props) => <p>Number of exercises {props.total}</p>
+import Total from "./Total/Total";
 
 const Course = ({ course }) => {
+    let totalExercises = 0
+    for(let i = 0; i < course.parts.length; i++) {
+        totalExercises += course.parts[i].exercises
+    }
 
-  return (
-    <div>
-      <Header name={course.name}/>
-      <Content parts={course.parts}/>
-      {/* <Total
-        total={
-          course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises
-        }
-      /> */}
-    </div>
-  )
+    return (
+        <div>
+            <Header name={course.name} />
+            <Content parts={course.parts} />
+            <Total total={totalExercises} />
+        </div>
+    )
 }
 
 export default Course
---
+++ part1/courseinfo/src/Course/Total/Total.jsx
@@ -0,0 +1,6 @@
+const Total = ({ total }) => {
+    console.log(total)
+    return <p><strong>Total of {total} exercises</strong></p>
+}
+
+export default Total
