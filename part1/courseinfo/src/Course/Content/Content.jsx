4e1c0ca9b10caa50330c4172835ccbd6d7e6fe41
2Tucho <albergromero@gmail.com>
2025-05-23 11:32:26 +0200

    Added exercise 2.5

--- part1/courseinfo/src/Course/Content/Content.jsx
+++ part1/courseinfo/src/Course/Content/Content.jsx
@@ -1,10 +1,8 @@
 import Part from "./Part/Part";
 
 const Content = ({ parts }) => {
-  console.log("parts", parts)
   return <>
     {parts.map(elem => {
-      console.log("elem.id", elem.id)
       return <div key={elem.id}>
         <Part part={elem} />
       </div>
