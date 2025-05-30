de4ad2baddf4cf8eb2408581ab5e93013d0e0472
2Tucho <albergromero@gmail.com>
2025-05-30 13:22:19 +0200

    Added exercise 2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+import countries from "./services/countries"
+
+function App() {
+  const [allCountries, setAllCountries] = useState([])
+  const [newCountry, setNewCountry] = useState([])
+
+  useEffect(() => {
+    countries
+      .getAll()
+      .then(allCountries => {
+        setAllCountries(allCountries)
+        setNewCountry(allCountries)
+      })
+  }, [])
+
+  const searchCountry = (event) => {
+    const filterCountry = allCountries.filter(elem => elem.name.common.toLowerCase().includes(event.target.value))
+    console.log("filterCountry", filterCountry);
+    setNewCountry(filterCountry)
+  }
+
+  return (
+    <>
+      <div>
+        Find countries <input onChange={searchCountry} />
+      </div>
+      {newCountry.length > 10 && newCountry.length < 249 ? 
+        <p>Too many matches, specify ahother filter</p>
+      : newCountry.length > 1 && newCountry.length <= 10 ?
+        newCountry.map((elem, i) => <p key={i}>{elem.name.common}</p>)
+      : newCountry.length == 1 ?
+        <div>
+          <h1>{newCountry[0].name.common}</h1>
+          <p>Capital: {newCountry[0].capital}</p>
+          <p>Area: {newCountry[0].area}</p>
+          <ul><h2>Lenguages</h2>
+            {Object.values(newCountry[0].languages).map((elem, i) => 
+              <li key={i}>{elem}</li>
+            )}
+          </ul>
+          <img src={newCountry[0].flags.png} alt={`${newCountry[0].name.common}'s flag`} />
+        </div>
+      : null}
+    </>
+  )
+}
+
+export default App
